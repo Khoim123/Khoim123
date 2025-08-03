d21ec4f502766fbe83661993a635487905edaa2a
Khoim123 <lethanhkhoi4408@gmail.com>
2025-08-04 01:40:24 +0700

    Update Blox Fruits.lua

--- Blox Fruits.lua
+++ Blox Fruits.lua
@@ -1,3 +1,12 @@
 if.game.PlaceId == 1754246095129 then
 
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+local Window = Fluent:CreateWindow({
+    Title = "Karb Hub V1.0 Beta" .. Fluent.Version,
+    SubTitle = "by dawid",
+    TabWidth = 160,
+    Size = UDim2.fromOffset(580, 460),
+    Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
+    Theme = "Dark",
+    MinimizeKey = Enum.KeyCode.LeftControl -- Used when theres no MinimizeKeybind
+})
