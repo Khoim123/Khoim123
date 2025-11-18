c54cfcf3d86b1c59f12944fe01268dabcfbd47b0
Khoim123 <lethanhkhoi4408@gmail.com>
2025-11-18 14:02:35 +0700

    Update LeThanhKhoihub.lua

--- LeThanhKhoihub.lua
+++ LeThanhKhoihub.lua
@@ -30,14 +30,13 @@ Fluent:Notify({
 -- Fluent provides Lucide Icons, they are optional
 local Tabs = {
     Main = Window:AddTab({ Title = "Main", Icon = "" }),
+Tab:AddButton({
+    Title = "Button",
+    Description = "Very important button",
+    Callback = function()
+        print("Hello, world!")
+    end
+})
     Test = Window:AddTab({ Title = "Test" , Icon = "" }),
     Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
 }
-
-Tab:AddButton({
-    Title = "Test",
-    Description = "Thử nghiệm",
-    Callback = function()
-        print("Thử nghiệm")
-    end
-})
