2de0b9a1300dec710453edabeee7799391337bf8
Khoim123 <lethanhkhoi4408@gmail.com>
2025-11-18 13:48:52 +0700

    Create LeThanhKhoihub.lua

---
+++ LeThanhKhoihub.lua
@@ -0,0 +1,25 @@
+local Library = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+
+local Window = Fluent:CreateWindow({
+    Title = "Le Thanh Khoi hub " .. V1.0,
+    SubTitle = "by Le Thanh Khoi",
+    TabWidth = 160,
+    Size = UDim2.fromOffset(580, 460),
+    Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
+    Theme = "Dark",
+    MinimizeKey = Enum.KeyCode.LeftControl -- Used when theres no MinimizeKeybind
+})
+
+Fluent:Notify({
+        Title = "Le Thanh Khoi Hub",
+        Content = "Đã được load thành công",
+        SubContent = "SubContent", -- Optional
+        Duration = 5 -- Set to nil to make the notification not disappear
+})
+
+-- Fluent provides Lucide Icons, they are optional
+local Tabs = {
+    Main = Window:AddTab({ Title = "Main", Icon = "" }),
+    Test = Window:Addtab({ Title = "Test" , Icon = "" }),
+    Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
+}
