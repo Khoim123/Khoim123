79fabe6c790e35b45fe8a8b11f857767cf091590
Khoim123 <lethanhkhoi4408@gmail.com>
2025-12-06 12:49:42 +0700

    Update kaitunbeta.lua

--- kaitunbeta.lua
+++ kaitunbeta.lua
@@ -1,25 +1,31 @@
+-- Chọn phe hải tặc hoặc hải quân
+task.wait(5)
 local args = {
 	"SetTeam",
-	"Pirates"
+	"Pirates"  --Pirates or Marines
 }
 game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("CommF_"):InvokeServer(unpack(args))
 local player = game.Players.LocalPlayer
 local character = player.Character or player.CharacterAdded:Wait()
 local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
 
--- Tìm NPC
-local npc = workspace.NPCs:FindFirstChild("Bandit Quest Giver")
+--Tween tới npc nhận nhiệm vụ hải tặc
+task.wait(5)
+local TweenService = game:GetService("TweenService")
+local player = game.Players.LocalPlayer
+local character = player.Character or player.CharacterAdded:Wait()
+local hrp = character:WaitForChild("HumanoidRootPart")
+local npc = workspace.NPCs["Bandit Quest Giver"]
 
-if npc and npc:FindFirstChild("HumanoidRootPart") then
-    -- Teleport tới vị trí NPC
-    humanoidRootPart.CFrame = npc.HumanoidRootPart.CFrame * CFrame.new(0, 0, 3)
-    print("Đã teleport tới Bandit Quest Giver")
-else
-    warn("Không tìm thấy NPC hoặc HumanoidRootPart của NPC")
-end
-local args = {
-	"StartQuest",
-	"BanditQuest1",
-	1
-}
-game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("CommF_"):InvokeServer(unpack(args))
+local speed = 325 -- Tốc độ (studs/giây) - chỉnh con số này
+local targetCFrame = npc.HumanoidRootPart.CFrame * CFrame.new(0, 0, 3)
+local distance = (hrp.Position - targetCFrame.Position).Magnitude
+local time = distance / speed
+
+local tween = TweenService:Create(hrp, TweenInfo.new(time), {
+    CFrame = targetCFrame
+})
+
+tween:Play()
+
+-nhận nhiệm vụ bandit
